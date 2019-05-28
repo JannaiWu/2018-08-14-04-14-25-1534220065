62a54e0d3cd8ef898fbc44df0cfbcb86be18ebb5
JannaiWu <45631329+JannaiWu@users.noreply.github.com>
2019-05-28 20:44:01 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,4 +1,30 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(n) {
+    //数字分为三层
+    var flag=[['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']];
+    var number=[];
+    //获取数字的每一位数字
+    while(true)
+    {
+        number.unshift(n%10);
+        n=parseInt(n/10);
+        if(n==0)
+        {
+            break;
+        }
+    }
+    //输出的字符根据数字分别组合，一共三行
+    var result='';
+    for(var i=0;i<3;i++)
+    {
+        for(var j=0;j<number.length;j++)
+        {
+            result+=flag[i][number[j]];
+            if(j!=number.length-1)
+            {
+                result+=' ';
+            }
+        }
+        result+='\n';
+    }
+    return result;
 };
